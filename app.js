1b669b6c9b26d84a98eb80c10d9a36475f1599b6
keitheck <eckertkeith@gmail.com>
2017-12-20 11:36:28 -0800

    code added

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var btn = document.getElementById('btn');
+var input = document.getElementById('item');
+var tasks = document.getElementById('tasks');
+
+if (localStorage.list){
+  var list = localStorage.list.split(',');
+} else {
+  var list = [];
+}
+
+function save() {
+  list.push(input.value);
+  localStorage.list = list;
+  console.log('list arr:', list);
+  console.log('localStorage list', localStorage.list);
+}
+
+function create() {
+  var val = inpjut.value;
+  var item = document.createElement('li');
+  item.appendChild(document.createTextNode(val));
+  tasks.appendChild(item);
+  input.value = ''; //resets input field
+}
+
+function load() {
+  if (localStorage.list) {
+    var item;
+    for (var x = 0; x < list.length; x++){
+      item = document.createElement('li');
+      item.appendChild(document.createTextNode(list[x]));
+      task.appendChild(item);
+    }
+  }
+}
+
+function render() {
+  save();
+  create();
+}
+
+load();
+
+btn.addEventListener("click", render() );
